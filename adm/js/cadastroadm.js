c4b588ca833bdd19508488c81a87083bbd4361fd
joaquimsen2306 <joaquimrafa2021@gmail.com>
2025-04-24 08:52:48 -0300

    att

--- adm/js/cadastroadm.js
+++ adm/js/cadastroadm.js
@@ -1,5 +1,4 @@
 document.getElementById("adminForm").addEventListener("submit", function (e) {
-<<<<<<< HEAD
   e.preventDefault();
 
   const nome = e.target.nome.value.trim();
@@ -40,25 +39,3 @@ document.getElementById("adminForm").addEventListener("submit", function (e) {
       alert("Erro ao cadastrar administrador.");
     });
 });
-=======
-    e.preventDefault();
-  
-    const nome = e.target.nome.value.trim();
-    const email = e.target.email.value.trim();
-    const senha = e.target.senha.value;
-    const confirmaSenha = e.target.confirmaSenha.value;
-    const telefone = e.target.telefone.value.trim();
-  
-    if (senha !== confirmaSenha) {
-      alert("As senhas não coincidem.");
-      return;
-    }
-  
-    // Aqui você pode salvar no localStorage, enviar para API, etc
-    alert("Administrador cadastrado com sucesso!\n" + nome);
-  
-    // Redirecionar para listagem
-    window.location.href = "../views/listagemadm.html";
-  });
-  
->>>>>>> 9cc2d757334244dd8bb9818361a526f037d7d28b
