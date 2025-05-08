ec99021fe244ba5354bc039460e7750560311de4
joaquimsen2306 <joaquimrafa2021@gmail.com>
2025-05-08 10:10:26 -0300

    a

--- adm/js/grafico.js
+++ adm/js/grafico.js
@@ -1,27 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const ctx = document.getElementById('graficoPizza').getContext('2d');
-    new Chart(ctx, {
-      type: 'pie',
-      data: {
-        labels: ['Alunos', 'Familiares', 'Infantil', 'Colaboradores', 'Comunidade'],
-        datasets: [{
-          data: [90, 50, 30, 50, 100],
-          backgroundColor: ['#66b3ff', '#ff9999', '#99ff99', '#ffcc99', '#c2c2f0'],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        plugins: {
-          legend: {
-            position: 'right',
-            labels: {
-              font: {
-                size: 14
-              }
-            }
-          }
-        }
+const ctx = document.getElementById('grafico').getContext('2d');
+
+const graficoPizza = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ['Alunos', 'Familiares', 'Infantil', 'Colaboradores', 'Comunidade'],
+    datasets: [{
+      data: [90, 50, 30, 50, 100],
+      backgroundColor: [
+        '#4bc0c0',
+        '#ff6384',
+        '#ffcd56',
+        '#36a2eb',
+        '#9966ff'
+      ]
+    }]
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom'
       }
-    });
-  });
-  
+    }
+  }
+});
